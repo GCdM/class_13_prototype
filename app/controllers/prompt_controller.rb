82a0df7e963b22b83a909dc85e29106ee0d40c7b
GCdM <59828466+GCdM@users.noreply.github.com>
2020-07-09 22:27:36 +0100

    Added prompt_id and participant_id to prompt#entry action to create Answer (to know if the person already clicked link)

--- app/controllers/prompt_controller.rb
+++ app/controllers/prompt_controller.rb
@@ -1,11 +1,20 @@
 class PromptController < ApplicationController
 
     def entry
+        @prompt = Prompt.find(params[:prompt_id])
+        @participant = Participant.find(params[:id])
+        @answer = Answer.find_or_create_by(prompt: @prompt, participant: @participant)
 
+        if @answer.audio.attached?
+            render :already_answered
+        else
+            render :entry
+        end
     end
 
     def answer
-
+        @answer = Answer.find(params[:id])
+        @prompt = @answer.prompt
     end
 
     def upload
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/entry', to: 'prompt#entry', as: 'entry'
-  get '/answer', to: 'prompt#answer', as: 'answer'
+  get '/prompt/:prompt_id/:id', to: 'prompt#entry', as: 'entry'
 
+  get '/answer/:id', to: 'prompt#answer', as: 'answer'
   post '/answer', to: 'prompt#upload'
 
   get '/dashboard', to: 'prompt#dashboard', as: 'dashboard'
